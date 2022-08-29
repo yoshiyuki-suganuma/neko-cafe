755b13f19a5c7161e163e8f9136f6acc94ade5b1
yoshiyuki-suganuma <109356197+yoshiyuki-suganuma@users.noreply.github.com>
2022-08-29 12:34:19 +0900

    add web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/contact', [ContactController::class,'index'])->name('contact');
+Route::post('/contact', [ContactController::class,'sendMail']);
+Route::get('/contact/complete', [ContactController::class,'complete'])->name('contact.complete');
